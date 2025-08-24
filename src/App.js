223496b1a4149dc520b05933be68f35cfced92b3
Tursunov Azizbek <agtursunov@gmail.com>
2025-08-24 19:35:50 +0500

    good

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold">Investor Portal Uzbekistan</h1>
+      <p className="mt-4">Добро пожаловать на инвестиционный портал</p>
+    </div>
+  );
+}
